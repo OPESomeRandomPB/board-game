8eee9f05c15500490834d81cfe8a7284c1b0a046
Oliver Peschka <ope.git@opeschka.de>
2021-11-20 21:23:14 +0100

    fixing typo

--- sql/mysql.table.game_sys_member.sql
+++ sql/mysql.table.game_sys_member.sql
@@ -2,7 +2,7 @@
 -- GNU GENERAL PUBLIC LICENSE
 -- origin of this project/file:
 --   https://github.com/OPESomeRandomPB/board-game
--- thanks a lot for all the people, which spread their knowlegde on several pages
+-- thanks a lot for all the people, which spread their knowledge on several pages
 --
 -- Script to create the game-group
 --
